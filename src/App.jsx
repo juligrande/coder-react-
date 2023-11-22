f2ccd240e52bce521ea7ebc26cdcbb10dae242e1
juligrande <grandejulif@gmail.com>
2023-11-21 23:16:59 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <NavBar/>
       <ItemListContainer greeting={"Bienvenido a mi Ecommerce"}/>
+      <ItemCount initial = {1} stock = {20} onAdd = {(quantity) => console.log('Cantidad Agregada', quantity)}/>
     </div>
   ); 
 }
