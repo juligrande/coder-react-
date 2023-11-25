e26aca1dff00ee3ae2e2d8eac82c062b44ec5e34
juligrande <grandejulif@gmail.com>
2023-11-24 21:07:34 -0300

    avances

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+import { getProducts } from '../../asyncMock'
+import ItemList from '../ItemList/ItemList'
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+
 const ItemListContainer = ({greeting}) =>{
+    const [products, setProducts] = useState([])
+    
+    const { categoryId } = useParams()
+
+    useEffect(() =>{
+        const asyncFunc = categoryId ? getProductsByCategory : getProducts
+
+        asyncFunc(categoryId)
+            .then(response => {
+                setProducts(response)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }, [categoryId])
+    
     return (
         <div>
             <h1>{greeting}</h1>
+            <ItemList products={products}/>
         </div>
     )
 
