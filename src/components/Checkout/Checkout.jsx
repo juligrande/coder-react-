6ca590afa77861ca9ed168088bd227083571c8a1
juligrande <grandejulif@gmail.com>
2023-12-28 14:32:37 -0300

    avances carrito

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,57 +1,28 @@
-import React, { useContext, useState } from "react";
-import { Timestamp, writeBatch, getDocs, query, where } from "firebase/firestore";
+import React, { useContext } from "react";
 import CartContext from "../CartContext/CartContext";
-import { db } from "../../firebase/client";
-import { addDoc, collection, documentId } from "firebase/firestore";
 import CheckoutForm from "../CheckoutForm/CheckoutForm";
 
-const CartItem = ({ product }) => {
-  return (
-    <div>
-      <p>{product.name} - Cantidad: {product.quantity} - Precio: ${product.price}</p>
-    </div>
-  );
-};
+const CartItem = ({ product }) => (
+  <div>
+    <p>
+      {product.name} - Cantidad: {product.quantity} - Precio: ${product.price}
+    </p>
+  </div>
+);
 
 const Checkout = () => {
-  const [loading, setLoading] = useState(false);
-  const [orderId, setOrderId] = useState('');
-  const { cart, clearCart } = useContext(CartContext);
-
-  const createOrder = async ({ name, phone, email }) => {
-    setLoading(true);
-
-    try {
-      const objOrder = {
-        buyer: {
-          name,
-          phone,
-          email
-        },
-        items: cart,
-      };
-
-
-      setOrderId("DAhds8232732hJHDSHJ"); 
-
-      clearCart();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { cart, total, clearCart } = useContext(CartContext);
 
   return (
     <div>
       <h1>Checkout</h1>
-      <CheckoutForm onConfirm={createOrder} />  
+      <CheckoutForm />
 
-      {cart.map(product => (
+      {cart.map((product) => (
         <CartItem key={product.id} product={product} />
       ))}
 
-      <p>Total: ${cart.reduce((total, product) => total + product.price * product.quantity, 0)}</p>
+      <p>Total: ${total}</p>
     </div>
   );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,37 @@
-import React, { useContext } from "react";
 import { NavLink, Link } from "react-router-dom";
-import "./NavBar.css";
+import { useContext } from "react";
+import './NavBar.css';
 import CartWidget from "../CartWidget/CartWidget";
 import CartContext from "../CartContext/CartContext";
 
 const NavBar = () => {
   const { cart } = useContext(CartContext);
 
-  const totalItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
+  const cartItemsCount = cart.reduce((acc, item) => acc + item.quantity, 0);
 
   return (
     <nav className="NavBar">
-      <Link to="/">
+      <Link to='/'>
         <h1 className="titulo">Ecommerce - Juligrande</h1>
       </Link>
       <div className="Categories">
-        <NavLink to="/" end className="Option">
+        <NavLink to='/' end className="Option">
           Inicio
         </NavLink>
-        <NavLink to="/item/1" className="Option">
+        <NavLink to='/item/1' className="Option">
           Iphone 12
         </NavLink>
-        <NavLink to="/item/2" className="Option">
+        <NavLink to='/item/2' className="Option">
           Samsung S21
         </NavLink>
-        <NavLink to="/item/3" className="Option">
+        <NavLink to='/item/3' className="Option">
           Ipad
         </NavLink>
-        <NavLink to="/cart">
-          <div className="cart-icon-container">
-            <img src="/src/components/CartWidget/assets/carrito.svg" alt="carrito" className="carrito" />
-            {totalItemsInCart > 0 && <span className="cart-item-count">{totalItemsInCart}</span>}
+        <NavLink to='/cart'>
+          <div className="cart-icon">
+            <img src="/src/components/CartWidget/assets/carrito.svg" alt="carrito" className="carrito"/>
+            {cartItemsCount > 0 && <span className="cart-count">{cartItemsCount}</span>}
           </div>
-        </NavLink>
-        <NavLink to="/checkout">
-          <button className="checkout-btn">Ir al Checkout</button>
         </NavLink>
       </div>
       <CartWidget />
